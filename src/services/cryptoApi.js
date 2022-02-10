2faabcaa4773ceeba46611e2c6fddbb576576da7
Jopibr <jopibr@gmail.com>
2022-02-10 20:48:30 -0300

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '4f96a6e58emsh575e71e659b032ap13150fjsn040c2321a983'
+    'x-rapidapi-key': '' // your API key
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
